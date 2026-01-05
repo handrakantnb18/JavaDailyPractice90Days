2408550ce435bb343f07b28a9849201c33329059
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-01-05 22:37:27 +0530

    checked For-Each loop statement

---
+++ src/com/januaryMonths/day3/JavaForEachLoop.java
@@ -0,0 +1,11 @@
+package com.januaryMonths.day3;
+
+public class JavaForEachLoop {
+    public static void main(String[] args) {
+        String[] froots = {"banana", "mango", "apple", };
+
+        for (String froot : froots) {
+            System.out.println(froot);
+        }
+    }
+}
