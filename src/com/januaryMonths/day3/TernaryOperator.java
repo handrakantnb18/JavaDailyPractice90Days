03adf0cbfe464a4c41de88c0409a020802790af7
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-01-05 20:35:28 +0530

    checked turnary operator

---
+++ src/com/januaryMonths/day3/TernaryOperator.java
@@ -0,0 +1,12 @@
+package com.januaryMonths.day3;
+
+public class TernaryOperator {
+    public static void main(String[] args) {
+
+        // check statement condition
+        int time = 12;
+
+        String result = (time < 15 ) ? "Good morning" : "Good  Afternoon..";
+        System.out.println(result);
+    }
+}
