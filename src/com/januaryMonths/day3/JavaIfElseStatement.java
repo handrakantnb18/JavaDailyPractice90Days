35de76176f4d2d2f6dc7d7c92631299e8f6c1857
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-01-05 20:09:07 +0530

    checked if and else statement check the value

---
+++ src/com/januaryMonths/day3/JavaIfElseStatement.java
@@ -0,0 +1,16 @@
+package com.januaryMonths.day3;
+
+public class JavaIfElseStatement {
+    public static void main(String[] args) {
+
+        // check the boolean value
+        boolean umbrella = true;
+
+        if (umbrella) {
+            System.out.println("Bring the Umbrella");
+        }else {
+            System.out.println("No rain, no Umbrella");
+        }
+
+    }
+}
