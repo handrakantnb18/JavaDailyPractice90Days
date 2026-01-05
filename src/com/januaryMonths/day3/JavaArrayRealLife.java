aaae5a3d613bfc15bb9b61b3256d90f831f93e3b
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-01-05 23:13:37 +0530

    checked Arrays real life loop statement

--- src/com/januaryMonths/day3/JavaArrayRealLife.java
+++ src/com/januaryMonths/day3/JavaArrayRealLife.java
@@ -2,6 +2,26 @@ package com.januaryMonths.day3;
 
 public class JavaArrayRealLife {
     public static void main(String[] args) {
+        int ages[] = {20, 25, 35, 65, 59, 58 ,78};
 
+        float avg, sum = 0;
+
+        int length = ages.length;
+
+        for (int age : ages){
+            sum += age;
+        }
+        avg = sum / length;
+
+        int lowestAge = 0;
+
+        for (int age : ages) {
+            if (lowestAge > age) {
+                lowestAge = age;
+            }
+            System.out.println("Lowest Age : "+lowestAge);
+        }
+
+        System.out.println("The average age is : "+avg);
     }
 }
