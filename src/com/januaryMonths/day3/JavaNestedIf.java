6643d2ee4ac55e5839fcd257c3520942dfec26a6
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-01-05 20:40:35 +0530

    checked Nested If condition

---
+++ src/com/januaryMonths/day3/JavaNestedIf.java
@@ -0,0 +1,19 @@
+package com.januaryMonths.day3;
+
+public class JavaNestedIf {
+    public static void main(String[] args) {
+
+        // check nested if condition
+        int num1 = 25;
+        int num2 = 40;
+
+        if (num1 > 18)
+        {
+            System.out.println("Num1 is grater than 25");
+        }
+        if (num2 > 35)
+        {
+            System.out.println("Num2 is grated than 40");
+        }
+    }
+}
