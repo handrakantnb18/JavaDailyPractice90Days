4d6d65d6e9556381b51f4353d0a65fb7a7c26d6e
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-01-05 20:18:31 +0530

    checked if and else If condition statement

---
+++ src/com/januaryMonths/day3/JavaIfElseIfStatement.java
@@ -0,0 +1,16 @@
+package com.januaryMonths.day3;
+
+public class JavaIfElseIfStatement {
+    public static void main(String[] args) {
+        // if else if check the greade and pass or not
+        int mark = 80;
+
+        if (mark == 80) {
+            System.out.println("A grade and pass..");
+        }else if (mark == 60){
+            System.out.println("B grade and pass..");
+        }else {
+            System.out.println("He is not pass..");
+        }
+    }
+}
