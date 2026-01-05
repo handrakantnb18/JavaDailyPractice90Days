bc4411a2ce36678b4f5275d693d73444743fb7c2
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-01-05 23:00:40 +0530

    checked Loop through an Arrays loop statement

---
+++ src/com/januaryMonths/day3/JavaLoopThroughArray.java
@@ -0,0 +1,12 @@
+package com.januaryMonths.day3;
+
+public class JavaLoopThroughArray {
+    public static void main(String[] args) {
+        String[] name = {"Chandrakant", "Priyanka", "Sachin", "Vijay", "Subhash"};
+
+        for (int i = 0; i < name.length; i++)
+        {
+            System.out.println(name[i]);
+        }
+    }
+}
