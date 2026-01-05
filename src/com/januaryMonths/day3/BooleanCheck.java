1dddc988ec763ebb59b016454b7cedba21b00e37
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-01-05 19:30:38 +0530

    checked boolean value true and false

--- src/com/januaryMonths/day3/BooleanCheck.java
+++ src/com/januaryMonths/day3/BooleanCheck.java
@@ -3,5 +3,15 @@ package com.januaryMonths.day3;
 public class BooleanCheck {
     public static void main(String[] args) {
         System.out.println("Hello world");
+
+        boolean b1 = true;
+        boolean b2 = false;
+        System.out.println(b1);
+        System.out.println(b2);
+
+        int x = 10;
+        int y = 20;
+        System.out.println(x > y);
+        System.out.println(x < y);
     }
 }
