5d5383f7fdf9437c5da146f2461cd4bb91e231b6
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-01-05 22:49:01 +0530

    checked Break/Continue loop statement

--- src/com/januaryMonths/day3/JavaBreakContinue.java
+++ src/com/januaryMonths/day3/JavaBreakContinue.java
@@ -27,5 +27,15 @@ public class JavaBreakContinue {
             }
             System.out.println(i);
         }
+
+        int i = 0;
+        while (i < 5) {
+            System.out.println(i);
+            i++;
+            if (i == 3){
+                break;
+            }
+        }
+
     }
 }
