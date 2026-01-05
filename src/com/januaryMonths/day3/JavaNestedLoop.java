064c2dbe9f8be662a07948bad44bc19e62ce3532
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-01-05 22:33:15 +0530

    checked Nested loop statement

---
+++ src/com/januaryMonths/day3/JavaNestedLoop.java
@@ -0,0 +1,12 @@
+package com.januaryMonths.day3;
+
+public class JavaNestedLoop {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 3; i++) {
+            for (int j = 1; j <= 3; j++) {
+                System.out.println(i * j +" ");
+            }
+            System.out.println();
+        }
+    }
+}
