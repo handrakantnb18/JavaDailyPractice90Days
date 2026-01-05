59505f005287157924e6a27ee6c807c2c3146218
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-01-05 19:49:36 +0530

    checked if statement

--- src/com/januaryMonths/day3/JavaIfElse.java
+++ src/com/januaryMonths/day3/JavaIfElse.java
@@ -2,10 +2,32 @@ package com.januaryMonths.day3;
 
 public class JavaIfElse {
     public static void main(String[] args) {
+
+        // check the boolean value
         boolean boo = true;
 
         if (boo) {
             System.out.println("Bring an umbrella!...");
         }
+
+        // check the greater than value
+        if (20 > 18) {
+            System.out.println("20 is greater than 18..");
+        }
+
+
+        int num1 = 25;
+        int num2 = 18;
+        if (num1 > num2) {
+            System.out.println("X is greater than Y..");
+        }
+
+        int a = 100;
+        int b = 100;
+        if (a == b){
+            System.out.println("A and B is equals..");
+        }
+
+
     }
 }
