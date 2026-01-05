05b3b6775afbc06a2523a92b4406bbf5c1023ab0
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-01-05 22:54:15 +0530

    checked Arrays loop statement

--- src/com/januaryMonths/day3/JavaArrayLoops.java
+++ src/com/januaryMonths/day3/JavaArrayLoops.java
@@ -2,7 +2,13 @@ package com.januaryMonths.day3;
 
 public class JavaArrayLoops {
     public static void main(String[] args) {
-        String[] cars = {"BMW", "Maruthi", "Ford"};
+        String[] cars = {"BMW", "Maruthi", "Ford", "Valvo"};
         System.out.println(cars[1]);
+
+        String[] cars1 = {"BMW", "Maruthi", "Ford", "Valvo"};
+        cars1[0] = "Leyland";
+        System.out.println(cars[1]);
+
+        System.out.println(cars1.length);
     }
 }
