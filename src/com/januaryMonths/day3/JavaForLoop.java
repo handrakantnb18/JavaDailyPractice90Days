710107c1923afc81431490a718bfd8e78a862175
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-01-05 22:24:28 +0530

    checked For loop statement

---
+++ src/com/januaryMonths/day3/JavaForLoop.java
@@ -0,0 +1,12 @@
+package com.januaryMonths.day3;
+
+public class JavaForLoop {
+    public static void main(String[] args) {
+
+        int i = 1;
+        for (i = 0; i < 5; i++)
+        {
+            System.out.println(i);
+        }
+    }
+}
