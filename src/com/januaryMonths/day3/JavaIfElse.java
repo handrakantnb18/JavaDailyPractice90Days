9a239c1d368d26b20e3aa1fbe0456a3fe0927a63
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-01-05 19:41:35 +0530

    checked if statement

---
+++ src/com/januaryMonths/day3/JavaIfElse.java
@@ -0,0 +1,11 @@
+package com.januaryMonths.day3;
+
+public class JavaIfElse {
+    public static void main(String[] args) {
+        boolean boo = true;
+
+        if (boo) {
+            System.out.println("Bring an umbrella!...");
+        }
+    }
+}
