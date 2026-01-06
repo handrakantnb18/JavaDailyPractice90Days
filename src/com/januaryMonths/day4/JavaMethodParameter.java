b067616586a5719a7c7f85408fc266d8d35ed554
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-01-06 15:00:38 +0530

    checked methods parameters

--- src/com/januaryMonths/day4/JavaMethodParameter.java
+++ src/com/januaryMonths/day4/JavaMethodParameter.java
@@ -3,6 +3,12 @@ package com.januaryMonths.day4;
 public class JavaMethodParameter {
     static void display(String fName, String lName, int age){
         System.out.println(fName + "chandrakant "+ lName + " "+ age + "  ");
+
+        if (age < 18 ) {
+            System.out.println("Access denied - you are not eligible...");
+        }else {
+            System.out.println("Access Granted - you are eligible......");
+        }
     }
     public static void main(String[] args) {
         display("Chandrakant ", " Bhosale ", 33);
