18f45fc5ca31fb25c0cdcf1fed5dd24374ebcadb
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-01-06 20:42:45 +0530

    Scope of variables

---
+++ src/com/januaryMonths/day4/JavaScope.java
@@ -0,0 +1,12 @@
+package com.januaryMonths.day4;
+
+public class JavaScope {
+    public static void main(String[] args) {
+        int x = 50;
+
+        for (x = 0; x < 5; x++) {
+            System.out.println("Loop : "+x);
+        }
+
+    }
+}
