a0345f6b5d920d63250d039c349e1febb70ec326
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-01-06 23:09:15 +0530

    Class and multiple object

---
+++ src/com/januaryMonths/day5/oops/First.java
@@ -0,0 +1,11 @@
+package com.januaryMonths.day5.oops;
+
+public class First {
+
+    public void fullTrottle() {
+        System.out.println("car is going fast..");
+    }
+    public void speed(int maxSpeed) {
+        System.out.println("Max speed : "+maxSpeed);
+    }
+}
---
+++ src/com/januaryMonths/day5/oops/Second.java
@@ -0,0 +1,9 @@
+package com.januaryMonths.day5.oops;
+
+public class Second {
+    public static void main(String[] args) {
+        First f = new First();
+        f.fullTrottle();
+        f.speed(150);
+    }
+}
