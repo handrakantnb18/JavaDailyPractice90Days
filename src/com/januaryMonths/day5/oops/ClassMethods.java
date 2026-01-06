8ccaad950558dee298fce8da9e5ce48ce35e6bb4
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-01-06 23:02:21 +0530

    Class and multiple object

--- src/com/januaryMonths/day5/oops/ClassMethods.java
+++ src/com/januaryMonths/day5/oops/ClassMethods.java
@@ -4,7 +4,19 @@ public class ClassMethods {
     static void display() {
         System.out.println("Chandrakant");
     }
+
+    public void fullThrottle() {
+        System.out.println("Car is going fast");
+    }
+
+    public void speed( int maxSpeed) {
+        System.out.println("MAx speed : "+maxSpeed);
+    }
+
     public static void main(String[] args) {
+        ClassMethods cm = new ClassMethods();
+        cm.fullThrottle();
+        cm.speed(100);
         System.out.println("Chandrakant bhosale");
         display();
     }
