30b539f281e1ea48f645199934757411609b2932
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-01-06 22:30:00 +0530

    Class and object and OOPS

--- src/com/januaryMonths/day5/oops/Main.java
+++ src/com/januaryMonths/day5/oops/Main.java
@@ -1,8 +1,12 @@
 package com.januaryMonths.day5.oops;
 
 public class Main {
+    int y = 25;
+
     public static void main(String[] args) {
         int x = 10;
+        Main m = new Main();
+        System.out.println(m.y);
         System.out.println(x);
     }
 }
