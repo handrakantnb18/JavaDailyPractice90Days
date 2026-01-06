6d9c1ab606d0a639e27fb6c528cebc48eb2cc3c9
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-01-06 14:49:18 +0530

    checked methods parameters

---
+++ src/com/januaryMonths/day4/JavaMethodParameter.java
@@ -0,0 +1,11 @@
+package com.januaryMonths.day4;
+
+public class JavaMethodParameter {
+    static void display(String fName){
+        System.out.println(fName + "chandrakant ");
+    }
+    public static void main(String[] args) {
+        display("Priyanka ");
+
+    }
+}
