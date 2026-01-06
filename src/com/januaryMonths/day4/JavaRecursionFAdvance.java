9339fbea1d13097db6c68bf58ede59f89862388c
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-01-06 21:06:06 +0530

    Recursion function advance

--- src/com/januaryMonths/day4/JavaRecursionFAdvance.java
+++ src/com/januaryMonths/day4/JavaRecursionFAdvance.java
@@ -2,9 +2,12 @@ package com.januaryMonths.day4;
 
 public class JavaRecursionFAdvance {
     static void contdown(int n) {
-        if (n > 0) {
+        if (n > 0 && n > 1) {
+            //return n * contdown(n - 1);
             System.out.println(n + " ");
             contdown(n - 1);
+        }else {
+                // return 1;
         }
     }
     public static void main(String[] args) {
