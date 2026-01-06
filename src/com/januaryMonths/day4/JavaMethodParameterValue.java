6ee6b39eae7ca27a462c49342d3a7ebbd029b415
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-01-06 16:09:45 +0530

    checked methods parameters Return values

--- src/com/januaryMonths/day4/JavaMethodParameterValue.java
+++ src/com/januaryMonths/day4/JavaMethodParameterValue.java
@@ -4,9 +4,16 @@ public class JavaMethodParameterValue {
     static int num(int a, int b) {
         return  a + b;
     }
+    static int doubleG (int x) {
+        return x * 2;
+    }
     public static void main(String[] args) {
+
         int c = num(4, 6);
         System.out.println(num(8, 7));
         System.out.println(c);
+        for (int i = 0; i <= 10; i++){
+            System.out.println("Double of "+ i + "is" + doubleG(i));
+        }
     }
 }
