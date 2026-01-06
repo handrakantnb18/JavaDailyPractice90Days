58d557770fa4bb0b85f8a26dd5d38941dfc9c696
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-01-06 22:55:45 +0530

    Class and multiple object

--- src/com/januaryMonths/day5/oops/ClassMethods.java
+++ src/com/januaryMonths/day5/oops/ClassMethods.java
@@ -1,7 +1,11 @@
 package com.januaryMonths.day5.oops;
 
 public class ClassMethods {
+    static void display() {
+        System.out.println("Chandrakant");
+    }
     public static void main(String[] args) {
         System.out.println("Chandrakant bhosale");
+        display();
     }
 }
