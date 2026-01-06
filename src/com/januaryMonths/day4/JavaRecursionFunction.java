20ef116cf9c8bfd160d4fe83d550d57db8138a54
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-01-06 20:53:42 +0530

    Recursion function

---
+++ src/com/januaryMonths/day4/JavaRecursionFunction.java
@@ -0,0 +1,15 @@
+package com.januaryMonths.day4;
+
+public class JavaRecursionFunction {
+    public static int sum(int k) {
+        if (k > 0) {
+            return k + sum(k - 1);
+        }else {
+            return 0;
+        }
+    }
+    public static void main(String[] args) {
+        int result = sum(10);
+        System.out.println(result);
+    }
+}
