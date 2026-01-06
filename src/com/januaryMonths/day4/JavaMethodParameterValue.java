3db5b7f6d8323dd14a52144d3a3ec32c1ac7a4b2
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-01-06 15:05:01 +0530

    checked methods parameters Return values

---
+++ src/com/januaryMonths/day4/JavaMethodParameterValue.java
@@ -0,0 +1,10 @@
+package com.januaryMonths.day4;
+
+public class JavaMethodParameterValue {
+    static int num(int a) {
+        return 10 + a;
+    }
+    public static void main(String[] args) {
+        System.out.println(num(4));
+    }
+}
