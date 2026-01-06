89bee6abe8d1af5cd9925c0eaf8e61c444f10b8c
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-01-06 23:15:00 +0530

    Class and multiple object

--- src/com/januaryMonths/day5/oops/MainConstructor.java
+++ src/com/januaryMonths/day5/oops/MainConstructor.java
@@ -3,11 +3,12 @@ package com.januaryMonths.day5.oops;
 public class MainConstructor {
     int x;
 
-    public MainConstructor() {
+    public MainConstructor(int y) {
         x = 5;
+        x = y;
     }
     public static void main(String[] args) {
-        MainConstructor mc = new MainConstructor();
+        MainConstructor mc = new MainConstructor(10);
         System.out.println(mc.x);
     }
 }
