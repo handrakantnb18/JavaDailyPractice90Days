93db74b11e734eada01a8848270d6ce2e7cfca53
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-01-08 21:35:19 +0530

    Java Inheritance

---
+++ src/com/januaryMonths/day6/oops2/AnimalPolym.java
@@ -0,0 +1,26 @@
+package com.januaryMonths.day6.oops2;
+
+class Animal {
+    public void animalSound() {
+        System.out.println("Animal make sound...");
+    }
+}
+
+class Pig extends Animal {
+    public void animalSound() {
+        System.out.println("Wee, Wee");
+    }
+}
+class Dog extends Pig {
+    public void animalSound() {
+        System.out.println("Bho, Bho...");
+    }
+}
+public class AnimalPolym  extends Dog {
+
+    public static void main(String[] args) {
+        AnimalPolym a = new AnimalPolym();
+        a.animalSound();
+        a.animalSound();
+    }
+}
