64e5d1f82db4c429d398eafb50b8bc89497c5142
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-01-08 21:20:46 +0530

    Package and API with Scanner function

--- src/com/januaryMonths/day6/oops2/Main.java
+++ src/com/januaryMonths/day6/oops2/Main.java
@@ -3,7 +3,7 @@ package com.januaryMonths.day6.oops2;
 public class Main {
     public static void main(String[] args) {
         Person person = new Person();
-        person.name = "Chandrakant bhosale";
-        System.out.println(person.name);
+        // person.name = "Chandrakant bhosale";
+        // System.out.println(person.name);
     }
 }
