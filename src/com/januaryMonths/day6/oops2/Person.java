4c582f81477a8e5af0869f68aa283ba495dfeb24
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-01-08 11:41:20 +0530

    Access Modifires final and static method

---
+++ src/com/januaryMonths/day6/oops2/Person.java
@@ -0,0 +1,23 @@
+package com.januaryMonths.day6.oops2;
+
+public class Person {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public String setName(String newName) {
+        this.name = newName;
+        return newName;
+    }
+
+    class Main {
+        public static void main(String[] args) {
+            Person p = new Person();
+            p.setName("Chandrakant");
+            System.out.println(p.getName());
+        }
+    }
+}
