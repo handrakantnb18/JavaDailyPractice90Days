941986d82efe3fc5224269105786c9458624004e
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-01-10 19:19:31 +0530

    java errors Logical error

---
+++ src/com/januaryMonths/day7/error/LogicalError.java
@@ -0,0 +1,12 @@
+package com.januaryMonths.day7.error;
+
+public class LogicalError {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 2;
+        int c = a - b;
+        int d = a + b;
+        System.out.println(c);
+        System.out.println(d);
+    }
+}
