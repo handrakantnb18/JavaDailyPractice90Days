987ac987e57efa0522d533a54806cf6b700710d5
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-01-10 19:15:01 +0530

    java errors dived by zero

---
+++ src/com/januaryMonths/day7/error/SimplebyZero.java
@@ -0,0 +1,10 @@
+package com.januaryMonths.day7.error;
+
+public class SimplebyZero {
+    public static void main(String[] args) {
+        int x = 10;
+        int b = 5;
+        int z = x / b;
+        System.out.println(z);
+    }
+}
