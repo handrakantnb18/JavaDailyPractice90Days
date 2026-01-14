9868fae1580cdba83bf27e6515cb40fc01702372
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-01-14 20:49:43 +0530

    Exception multiple catch Arithmetic Exception

--- src/com/januaryMonths/Day8/exception/Main.java
+++ src/com/januaryMonths/Day8/exception/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         try {
             int[] numbers = {1, 2, 3};
-            System.out.println(numbers[10]);
+            System.out.println(numbers[9]);
             int result = 10 /0;
         }
         catch (ArrayIndexOutOfBoundsException e)
