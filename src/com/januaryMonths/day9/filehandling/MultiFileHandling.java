69e7edbc17d64487c60c2d295dafc9cce1733cab
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-01-14 22:23:11 +0530

    create multiple file handling index.txt

---
+++ src/com/januaryMonths/day9/filehandling/MultiFileHandling.java
@@ -0,0 +1,7 @@
+package com.januaryMonths.day9.filehandling;
+
+public class MultiFileHandling {
+    public static void main(String[] args) {
+        System.out.println("Multiple file handling...");
+    }
+}
