14d37505dc76987566951866380eba406f2a7470
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-01-14 21:06:07 +0530

    multi catch Arithmetic and ArrayIndexOutOfBoundsException Exception

---
+++ src/com/januaryMonths/Day8/exception/MultiCatchException.java
@@ -0,0 +1,14 @@
+package com.januaryMonths.Day8.exception;
+
+public class MultiCatchException {
+    public static void main(String[] args) {
+        try {
+            int result = 10/0;
+            int[] numbers = {1, 2, 3, 4, 5};
+            System.out.println(numbers[10]);
+        }catch (ArithmeticException | ArrayIndexOutOfBoundsException e) {
+            System.out.println("Math error divide by zero");
+        }
+    }
+
+}
