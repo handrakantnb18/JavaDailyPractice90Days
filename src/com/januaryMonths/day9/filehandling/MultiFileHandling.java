e69ef13a0af7fa8be6794e89cded3a323816d283
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-01-15 11:29:08 +0530

    create new file IOException index.txt

--- src/com/januaryMonths/day9/filehandling/MultiFileHandling.java
+++ src/com/januaryMonths/day9/filehandling/MultiFileHandling.java
@@ -1,7 +1,22 @@
 package com.januaryMonths.day9.filehandling;
 
+import java.io.File;
+import java.io.IOException;
+
 public class MultiFileHandling {
     public static void main(String[] args) {
         System.out.println("Multiple file handling...");
+        try {
+            File obj = new File("index.txt");
+            if (obj.createNewFile()) {
+                System.out.println("File created : "+obj.getName());
+            }else
+            {
+                System.out.println("File already existed.");
+            }
+        }catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
     }
 }
