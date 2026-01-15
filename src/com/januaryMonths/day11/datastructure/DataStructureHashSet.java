761effbea6cc044125f5502f935fbe167a75585c
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-01-15 15:19:48 +0530

    Data structure in HashSet check the duplicate allow

---
+++ src/com/januaryMonths/day11/datastructure/DataStructureHashSet.java
@@ -0,0 +1,17 @@
+package com.januaryMonths.day11.datastructure;
+
+import java.util.HashMap;
+import java.util.HashSet;
+
+public class DataStructureHashSet {
+    public static void main(String[] args) {
+        HashSet<String> hashSet = new HashSet<>();
+        hashSet.add("Chandraaknt");
+        hashSet.add("Priyanka");
+        hashSet.add("Vijay");
+        hashSet.add("Subhash");
+        hashSet.add("Chandrakant");
+
+        System.out.println(hashSet);
+    }
+}
