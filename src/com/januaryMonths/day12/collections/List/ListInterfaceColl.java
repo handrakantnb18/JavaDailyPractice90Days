8cd69b3500896a39e70e13a4227763b24b8611c3
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-01-15 20:15:40 +0530

    Collections List ArrayList

---
+++ src/com/januaryMonths/day12/collections/List/ListInterfaceColl.java
@@ -0,0 +1,16 @@
+package com.januaryMonths.day12.collections.List;
+
+import java.util.ArrayList;
+
+public class ListInterfaceColl {
+    public static void main(String[] args) {
+        ArrayList<String> arrayList = new ArrayList<>();
+        arrayList.add("Apple");
+        arrayList.add("Banana");
+        arrayList.add("Mango");
+        arrayList.add("Graps");
+        arrayList.add("Badam");
+
+        System.out.println(arrayList);
+    }
+}
