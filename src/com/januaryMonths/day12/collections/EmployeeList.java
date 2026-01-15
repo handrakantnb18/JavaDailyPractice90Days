1218b8ccdad20b71c0df5fe662c3ec39bf1180d2
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-01-15 20:16:03 +0530

    Collections List ArrayList Employee list

---
+++ src/com/januaryMonths/day12/collections/EmployeeList.java
@@ -0,0 +1,17 @@
+package com.januaryMonths.day12.collections;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class EmployeeList {
+    public static void main(String[] args) {
+        ArrayList<String> list = new ArrayList<>();
+        list.add("Chandrakant");
+        list.add("Priyanka");
+        list.add("Vijay");
+        list.add("Subhash");
+        list.add("Sachin");
+
+        System.out.println(list);
+    }
+}
