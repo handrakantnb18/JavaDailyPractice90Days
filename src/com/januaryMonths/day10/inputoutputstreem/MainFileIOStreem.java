9e845f120e036234ee73caad010711bb852ed822
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-01-15 13:17:15 +0530

    file input output stream read the file and count the charactors

---
+++ src/com/januaryMonths/day10/inputoutputstreem/MainFileIOStreem.java
@@ -0,0 +1,17 @@
+package com.januaryMonths.day10.inputoutputstreem;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class MainFileIOStreem {
+    public static void main(String[] args) {
+        try (FileInputStream input = new FileInputStream("abc.txt")){
+            int i;
+            while ((i = input.read()) != -1) {
+                System.out.print((char) i);
+            }
+        }catch (IOException e) {
+            System.out.println("Error reading file.");
+        }
+    }
+}
