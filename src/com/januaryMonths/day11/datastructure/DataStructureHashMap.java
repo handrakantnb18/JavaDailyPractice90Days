750a6789c8335a4133d1bb74c7bde7309e6bcb36
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-01-15 15:23:39 +0530

    Data structure in HashMap check the key and value pair

---
+++ src/com/januaryMonths/day11/datastructure/DataStructureHashMap.java
@@ -0,0 +1,15 @@
+package com.januaryMonths.day11.datastructure;
+
+import java.util.HashMap;
+
+public class DataStructureHashMap {
+    public static void main(String[] args) {
+        HashMap<String, String> hashMap = new HashMap<>();
+        hashMap.put("Rohit", "Sharma");
+        hashMap.put("Virat", "kohli");
+        hashMap.put("rushabh", "pant");
+        hashMap.put("KL", "Rahul");
+
+        System.out.println(hashMap);
+    }
+}
