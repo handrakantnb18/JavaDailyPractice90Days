e82aeeedd47366ca325b909704b0d81df3333f46
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-01-15 15:15:43 +0530

    Data structure in ArrayList

---
+++ src/com/januaryMonths/day11/datastructure/DataStructure.java
@@ -0,0 +1,15 @@
+package com.januaryMonths.day11.datastructure;
+
+import java.util.ArrayList;
+
+public class DataStructure {
+    public static void main(String[] args) {
+        ArrayList<String> cars = new ArrayList<>();
+        cars.add("TATA");
+        cars.add("KIYA");
+        cars.add("BMW");
+        cars.add("ERTIGA");
+
+        System.out.println(cars);
+    }
+}
