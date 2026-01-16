f67b11c37864bdd8901dfe02aa349a4e0852f54f
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-01-16 23:20:24 +0530

    LinhedHashMap method

---
+++ src/com/januaryMonths/day12/collections/LinkedHashMap/Main.java
@@ -0,0 +1,16 @@
+package com.januaryMonths.day12.collections.LinkedHashMap;
+
+import java.util.LinkedHashMap;
+
+public class Main {
+    public static void main(String[] args) {
+        LinkedHashMap<String, String> capitlas = new LinkedHashMap<>();
+        capitlas.put("Maharashtra ", " Mumbai");
+        capitlas.put("Karnataka ", " Banglore");
+        capitlas.put("Andra pradesh ", " Hydrabad");
+        capitlas.put("Goa ", " Panaji");
+        capitlas.put("Tamilnadu ", " Chennai");
+
+        System.out.println(capitlas);
+    }
+}
