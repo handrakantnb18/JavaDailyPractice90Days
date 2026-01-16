f6194c3d6939dfe40149a731bd821ef3c21182c9
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-01-16 21:45:26 +0530

    Array list simple example

---
+++ src/com/januaryMonths/day12/collections/ArrayList/Student.java
@@ -0,0 +1,16 @@
+package com.januaryMonths.day12.collections.ArrayList;
+
+import java.util.ArrayList;
+
+public class Student {
+    public static void main(String[] args) {
+        ArrayList<String> arrayList = new ArrayList<>();
+        arrayList.add("Chandrakant");
+        arrayList.add("Priyanka");
+        arrayList.add("Rahul");
+        arrayList.add("Ram");
+        arrayList.add("Shyam");
+
+        System.out.println(arrayList);
+    }
+}
