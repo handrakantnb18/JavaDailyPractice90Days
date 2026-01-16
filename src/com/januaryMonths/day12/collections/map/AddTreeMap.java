677d77223b0747919bc21a8ad27a85aeccc18db9
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-01-16 23:11:17 +0530

    TreeMap check String key value pairs

---
+++ src/com/januaryMonths/day12/collections/map/AddTreeMap.java
@@ -0,0 +1,16 @@
+package com.januaryMonths.day12.collections.map;
+
+import java.util.TreeMap;
+
+public class AddTreeMap {
+    public static void main(String[] args) {
+        TreeMap<String, String> capitlas = new TreeMap<>();
+        capitlas.put("Maharashtra ", " Mumbai");
+        capitlas.put("Karnataka ", " Banglore");
+        capitlas.put("Andra pradesh ", " Hydrabad");
+        capitlas.put("Goa ", " Panaji");
+        capitlas.put("Tamilnadu ", " Chennai");
+
+        System.out.println(capitlas);
+    }
+}
