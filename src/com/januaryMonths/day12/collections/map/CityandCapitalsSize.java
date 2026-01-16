130cec0232854ded69be6d733b403ed4f27e6cbd
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-01-16 22:57:58 +0530

    HsahMap size method() String key value pairs

---
+++ src/com/januaryMonths/day12/collections/map/CityandCapitalsSize.java
@@ -0,0 +1,18 @@
+package com.januaryMonths.day12.collections.map;
+
+import java.util.HashMap;
+
+public class CityandCapitalsSize {
+    public static void main(String[] args) {
+        HashMap<String, String> capitlas = new HashMap<String, String>();
+        capitlas.put("Maharashtra ", " Mumbai");
+        capitlas.put("Karnataka ", " Banglore");
+        capitlas.put("Andra pradesh ", " Hydrabad");
+        capitlas.put("Goa ", " Panaji");
+        capitlas.put("Tamilnadu ", " Chennai");
+
+        System.out.println(capitlas);
+
+        System.out.println(capitlas.size());
+    }
+}
