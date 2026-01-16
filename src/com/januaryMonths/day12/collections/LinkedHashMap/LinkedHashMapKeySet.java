7e106c1136ccbd18771ef0881f0d671afc8f2369
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-01-16 23:25:05 +0530

    LinhedHashMap keySet() values and key remove() method and size checked

---
+++ src/com/januaryMonths/day12/collections/LinkedHashMap/LinkedHashMapKeySet.java
@@ -0,0 +1,20 @@
+package com.januaryMonths.day12.collections.LinkedHashMap;
+
+import java.util.LinkedHashMap;
+
+public class LinkedHashMapKeySet {
+    public static void main(String[] args) {
+        LinkedHashMap<String, String> capitlas = new LinkedHashMap<>();
+        capitlas.put("Maharashtra ", " Mumbai");
+        capitlas.put("Karnataka ", " Banglore");
+        capitlas.put("Andra pradesh ", " Hydrabad");
+        capitlas.put("Goa ", " Panaji");
+        capitlas.put("Tamilnadu ", " Chennai");
+
+        for (String key : capitlas.keySet()) {
+            System.out.println(key);
+        }
+        capitlas.remove("Andra pradesh ", " Hydrabad");
+        System.out.println(capitlas.size());
+    }
+}
