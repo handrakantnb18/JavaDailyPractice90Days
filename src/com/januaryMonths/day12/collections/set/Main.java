4759fc2b11a52051db650beed9c64e70b8bc5224
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-01-16 22:22:32 +0530

    HashMap Integer and String collections

---
+++ src/com/januaryMonths/day12/collections/set/Main.java
@@ -0,0 +1,16 @@
+package com.januaryMonths.day12.collections.set;
+
+import java.util.HashMap;
+
+public class Main {
+    public static void main(String[] args) {
+        HashMap<Integer, String> hm = new HashMap<Integer, String>();
+        hm.put(101, " Chandrakant");
+        hm.put(201, " Priyanka");
+        hm.put(301, " Vijay");
+        hm.put(401, " Ram");
+        hm.put(501, " Shyam");
+
+        System.out.println(hm);
+    }
+}
