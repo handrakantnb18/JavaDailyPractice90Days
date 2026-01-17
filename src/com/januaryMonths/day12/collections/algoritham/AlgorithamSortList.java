26d651c6d6b3b17df3316d0c0d6887bf8d720f0c
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-01-17 21:45:37 +0530

    Algorithm binary search and sort

---
+++ src/com/januaryMonths/day12/collections/algoritham/AlgorithamSortList.java
@@ -0,0 +1,20 @@
+package com.januaryMonths.day12.collections.algoritham;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class AlgorithamSortList {
+    public static void main(String[] args) {
+        ArrayList<String> city = new ArrayList<>();
+        city.add("Mumbai");
+        city.add("Pune");
+        city.add("Banglore");
+        city.add("Dilhi");
+        city.add("Panjab");
+
+        Collections.sort(city);
+        int a = Collections.binarySearch(city, "Chennai");
+        System.out.println("By name : " +a);
+        System.out.println(city);
+    }
+}
