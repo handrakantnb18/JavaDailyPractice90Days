91aca6d79f6348c652660245826fa04f2590408e
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-01-17 22:06:54 +0530

    Algorithm sort

---
+++ src/com/januaryMonths/day12/collections/algoritham/AlgorithamReverseOrder.java
@@ -0,0 +1,20 @@
+package com.januaryMonths.day12.collections.algoritham;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class AlgorithamReverseOrder {
+    public static void main(String[] args) {
+        ArrayList<Integer> num1 = new ArrayList<>();
+        num1.add(5);
+        num1.add(1);
+        num1.add(4);
+        num1.add(3);
+        num1.add(2);
+        num1.add(7);
+        num1.add(6);
+
+        Collections.sort(num1);
+        System.out.println(num1);
+    }
+}
