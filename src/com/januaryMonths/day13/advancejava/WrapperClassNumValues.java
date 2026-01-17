735c96213fe28014c6aabc7709785f51a796e500
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-01-17 22:44:57 +0530

    Advance java Wrapper Class values method

---
+++ src/com/januaryMonths/day13/advancejava/WrapperClassNumValues.java
@@ -0,0 +1,15 @@
+package com.januaryMonths.day13.advancejava;
+
+public class WrapperClassNumValues {
+    public static void main(String[] args) {
+        Integer num = 10;
+        Double myDouble = 45.00;
+        Character character = 'B';
+        float abc = 33.3f;
+
+        System.out.println(num.intValue());
+        System.out.println(myDouble.doubleValue());
+        System.out.println(character.charValue());
+        System.out.println(abc);
+    }
+}
