f84759aeee0e695d6b21c414aa47ebacc7f5f60b
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-01-17 22:50:49 +0530

    Advance java Wrapper Class toString() method multi data types

---
+++ src/com/januaryMonths/day13/advancejava/WrapperClassToStringMe.java
@@ -0,0 +1,15 @@
+package com.januaryMonths.day13.advancejava;
+
+public class WrapperClassToStringMe {
+    public static void main(String[] args) {
+        Integer n = 400;
+        String a = "chandrakant";
+
+        String st = n.toString();
+        String sr = a.toString();
+
+        System.out.println(st.length());
+        System.out.println(sr);
+        System.out.println(sr.length());
+    }
+}
