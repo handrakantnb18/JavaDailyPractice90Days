f517d7b92d183f6d940aa39db881f26a229477ef
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-01-17 22:47:35 +0530

    Advance java Wrapper Class toString() method

---
+++ src/com/januaryMonths/day13/advancejava/WrapperClassToString.java
@@ -0,0 +1,9 @@
+package com.januaryMonths.day13.advancejava;
+
+public class WrapperClassToString {
+    public static void main(String[] args) {
+        Integer n = 400;
+        String st = n.toString();
+        System.out.println(st.length());
+    }
+}
