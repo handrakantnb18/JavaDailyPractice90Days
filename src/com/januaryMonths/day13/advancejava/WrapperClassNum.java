3937d6017e7779293d8e9177322133431743c1ed
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-01-17 22:39:58 +0530

    Advance java Wrapper Class

---
+++ src/com/januaryMonths/day13/advancejava/WrapperClassNum.java
@@ -0,0 +1,13 @@
+package com.januaryMonths.day13.advancejava;
+
+public class WrapperClassNum {
+    public static void main(String[] args) {
+        Integer num = 50;
+        Double numDouble = 55.50;
+        Character character = 'A';
+        System.out.println(num);
+        System.out.println(numDouble);
+        System.out.println(character);
+
+    }
+}
