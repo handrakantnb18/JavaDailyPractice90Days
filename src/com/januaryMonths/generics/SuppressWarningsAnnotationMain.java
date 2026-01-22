8fc7f9b6e5c1662ffbacfd14ecc5cd6f7801f6b1
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-01-22 23:07:39 +0530

    Annotations Main @SuppressWarnings Annotation

---
+++ src/com/januaryMonths/generics/SuppressWarningsAnnotationMain.java
@@ -0,0 +1,11 @@
+package com.januaryMonths.generics;
+
+import java.util.ArrayList;
+
+public class SuppressWarningsAnnotationMain {
+    public static void main(String[] args) {
+        ArrayList cars = new ArrayList();
+        cars.add("Volvo");
+        System.out.println(cars);
+    }
+}
