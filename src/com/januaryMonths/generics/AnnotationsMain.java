e7356888d8c6052751031286e3b8f0015e912f41
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-01-22 23:01:59 +0530

    Annotations Main @Override Annotation

---
+++ src/com/januaryMonths/generics/AnnotationsMain.java
@@ -0,0 +1,21 @@
+package com.januaryMonths.generics;
+
+class Animal {
+    void makeSound() {
+        System.out.println("Animal sound");
+    }
+}
+
+class Dog extends Animal {
+    @Override
+    void makesound() { // Typo here!
+        System.out.println("Woof!");
+    }
+}
+
+public class AnnotationsMain {
+    public static void main(String[] args) {
+        Animal myDog = new Animal();
+        myDog.makeSound();
+    }
+}
