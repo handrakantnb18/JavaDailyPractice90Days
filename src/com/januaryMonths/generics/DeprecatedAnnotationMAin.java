3a0a961a91a9e2481382649968cf5466b6e14b39
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-01-22 23:05:51 +0530

    Annotations Main @Deprecated Annotation

---
+++ src/com/januaryMonths/generics/DeprecatedAnnotationMAin.java
@@ -0,0 +1,11 @@
+package com.januaryMonths.generics;
+
+public class DeprecatedAnnotationMAin {
+
+    static void oldMethod() {
+        System.out.println("This method is outdated...");
+    }
+    public static void main(String[] args) {
+        oldMethod();
+    }
+}
