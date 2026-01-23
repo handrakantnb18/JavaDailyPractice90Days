612d3a590f102c6c725456d5d6b97b691a2c116c
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-01-23 21:13:26 +0530

    Comparators to sort a list of cars by year Advanced Sorting

---
+++ src/com/januaryMonths/day16/advancedsorting/SortByYear.java
@@ -0,0 +1,48 @@
+package com.januaryMonths.day16.advancedsorting;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
+class Car {
+    public String brand;
+    public String model;
+    public int year;
+
+    public Car(String b, String m, int y) {
+        brand = b;
+        model = m;
+        year = y;
+    }
+}
+
+class Main implements Comparator {
+    public int compare(Object obj1, Object obj2) {
+        Car a = (Car) obj1;
+        Car b = (Car) obj2;
+
+        if (a.year < b.year)
+            return -1;
+
+        if (a.year > b.year)
+            return 1;
+
+        return 0;
+    }
+}
+
+public class SortByYear {
+    public static void main(String[] args) {
+        ArrayList<Car> myCars = new ArrayList<Car>();
+        myCars.add(new Car("Bmw", "X5", 1999));
+        myCars.add(new Car("Honda", "Accord", 2006));
+        myCars.add(new Car("Ford", "Mustang", 1970));
+
+        Comparator myC = new Main();
+        Collections.sort(myCars, myC);
+
+        for (Car c : myCars) {
+            System.out.println(c.brand + " " +c.model + " " + c.year);
+        }
+    }
+}
