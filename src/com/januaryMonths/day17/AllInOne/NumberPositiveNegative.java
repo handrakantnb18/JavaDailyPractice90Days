9d6a46661acfe99ab5678f09a5fb29198ecc953e
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-01-23 22:59:28 +0530

    Find Out if a Number is Positive or Negative

---
+++ src/com/januaryMonths/day17/AllInOne/NumberPositiveNegative.java
@@ -0,0 +1,17 @@
+package com.januaryMonths.day17.AllInOne;
+
+public class NumberPositiveNegative {
+    public static void main(String[] args) {
+        int num = 10;
+
+        if (num > 0) {
+            System.out.println("The number is positive");
+        }
+        else if (num < 0) {
+            System.out.println("The value is negative number.");
+        }
+        else {
+            System.out.println("The value is 0");
+        }
+    }
+}
