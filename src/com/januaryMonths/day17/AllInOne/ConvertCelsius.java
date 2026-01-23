654469bdf222b6fa7a9ea105e94b7b3b6484ca26
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-01-23 23:04:27 +0530

    To - Convert Celsius to Fahrenheit

---
+++ src/com/januaryMonths/day17/AllInOne/ConvertCelsius.java
@@ -0,0 +1,10 @@
+package com.januaryMonths.day17.AllInOne;
+
+public class ConvertCelsius {
+    public static void main(String[] args) {
+        double celsius = 23.5;
+        double fahrenheit = celsius * 9 / 5 + 32;
+
+        System.out.println(celsius + " C = " + fahrenheit + " F ");
+    }
+}
