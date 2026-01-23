56f5c8e874cfcd477971166ff7c0843accd502fa
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-01-23 23:02:06 +0530

    To Get the Area of a Rectangle

---
+++ src/com/januaryMonths/day17/AllInOne/AreaRectangle.java
@@ -0,0 +1,10 @@
+package com.januaryMonths.day17.AllInOne;
+
+public class AreaRectangle {
+    public static void main(String[] args) {
+        int length = 5;
+        int width = 2;
+        int area = length * width;
+        System.out.println("Area of the rectangle : "+ area);
+    }
+}
