78fb2510673a89af90f683c62f1e31053e69b598
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-01-23 22:53:59 +0530

    Check Whether a Number is Even or Odd

---
+++ src/com/januaryMonths/day17/AllInOne/OddEvenNum.java
@@ -0,0 +1,13 @@
+package com.januaryMonths.day17.AllInOne;
+
+public class OddEvenNum {
+    public static void main(String[] args) {
+        int number = 5;
+
+        if (number % 2 == 0) {
+            System.out.println(number + " Is Even Number.");
+        }else {
+            System.out.println(number + " Is Odd Number.");
+        }
+    }
+}
