131c24e23d9ba331164c6711da3241549021af31
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-01-24 22:06:17 +0530

    To Convert a String to an Array

---
+++ src/com/januaryMonths/day18/AllIntwo/ConvertaStringArray.java
@@ -0,0 +1,11 @@
+package com.januaryMonths.day18.AllIntwo;
+
+public class ConvertaStringArray {
+    public static void main(String[] args) {
+        String str = "Chandrakant";
+
+        char[] arr = str.toCharArray();
+
+        System.out.println(arr[0]);
+    }
+}
