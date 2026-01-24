f93c24af0572c8d4179ace836914149e7b936de1
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-01-24 22:09:23 +0530

    To - Remove Whitespace from a String

---
+++ src/com/januaryMonths/day18/AllIntwo/RemoveWhitespaceString.java
@@ -0,0 +1,10 @@
+package com.januaryMonths.day18.AllIntwo;
+
+public class RemoveWhitespaceString {
+    public static void main(String[] args) {
+        String t = "    bhosale ";
+        String trimmed = t.trim();
+
+        System.out.println(trimmed);
+    }
+}
