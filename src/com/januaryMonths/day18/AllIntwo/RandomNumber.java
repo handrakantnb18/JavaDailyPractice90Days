70a38b496c9f072dd4663994b516d3081b6f000c
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-01-24 17:18:43 +0530

    To Generate Random Numbers

---
+++ src/com/januaryMonths/day18/AllIntwo/RandomNumber.java
@@ -0,0 +1,9 @@
+package com.januaryMonths.day18.AllIntwo;
+
+public class RandomNumber {
+    public static void main(String[] args) {
+        int a = 1;
+        int randomNum = (int) (Math.random() * 10);
+        System.out.println(randomNum * a);
+    }
+}
