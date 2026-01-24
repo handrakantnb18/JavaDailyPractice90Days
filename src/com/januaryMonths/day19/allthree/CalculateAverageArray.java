17923a31210742123f5a0d5d89a73757ee2d77f4
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-01-24 22:20:24 +0530

    To Calculate the Average of Array Elements

---
+++ src/com/januaryMonths/day19/allthree/CalculateAverageArray.java
@@ -0,0 +1,19 @@
+package com.januaryMonths.day19.allthree;
+
+public class CalculateAverageArray {
+    public static void main(String[] args) {
+        int ages[] = {20, 22, 18, 35, 48, 26, 87, 70};
+
+        float avg, sum = 0;
+
+        int length = ages.length;
+
+        for (int age : ages) {
+            sum += age;
+        }
+
+        avg = sum / length;
+
+        System.out.println("The Average age is : " +avg);
+    }
+}
