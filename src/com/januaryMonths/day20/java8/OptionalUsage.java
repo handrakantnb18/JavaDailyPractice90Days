f552442713552d6a4cd36a19cdf25fb9bb0c2c0d
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-01-26 15:55:18 +0530

    Java 8 stream() 8. Optional usage  Using empty() and of()

--- src/com/januaryMonths/day20/java8/OptionalUsage.java
+++ src/com/januaryMonths/day20/java8/OptionalUsage.java
@@ -6,23 +6,34 @@ public class OptionalUsage {
     public static void main(String[] args) {
 
         // Java program without the Optional Class
-
-        String[] words = new String[10];
-        String word = words[5].toLowerCase();
-        System.out.println(word);
+//
+//        String[] words = new String[10];
+//        String word = words[5].toLowerCase();
+//        System.out.println(word);
 
         // Program using Optional Class
 
-        String[] words1 = new String[10];
+        String[] w = new String[10];
 
-        Optional<String> checkNull = Optional.ofNullable(words1[5]);
+        Optional<String> checkNull = Optional.ofNullable(w[5]);
 
         if (checkNull.isPresent()) {
-            String word1 = words1[5].toLowerCase();
-            System.out.print(word1);
+            String wd = w[5].toLowerCase();
+            System.out.print(wd);
         }
         else
-            System.out.println("Word is full.");
+            System.out.println("word is null");
+
+        //  Using empty() and of()
+
+        String[] str = new String[5];
+        str[2] = "Priyanka Chandraknt Bhosale Sangli Pune I Love You";
+
+        Optional<String> empty = Optional.empty();
+        System.out.println(empty);
+
+        Optional<String> value = Optional.of(str[2]);
+        System.out.println(value);
     }
 
 }
